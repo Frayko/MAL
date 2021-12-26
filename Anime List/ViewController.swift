5ce258c6ccac93e21f8f5ad803c207e93a12d449
Alex <sasha.1.sas@mail.ru>
2021-12-26 18:16:37 +0700

    Initial Commit

---
+++ Anime List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anime List
+//
+//  Created by Александр Фомин on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
