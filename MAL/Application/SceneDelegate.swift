465b89738eea0d1ce8b62dca19113a70258d9299
Alex <sasha.1.sas@mail.ru>
2021-12-26 18:37:04 +0700

    Создал проект

---
+++ MAL/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  MAL
+//
+//  Created by Александр Фомин on 26.12.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: windowScene)
+		let viewController = UIViewController()//LoadImagesAssembly.build()
+		window.rootViewController = viewController
+		self.window = window
+		self.window?.makeKeyAndVisible()
+	}
+
+	func sceneDidEnterBackground(_ scene: UIScene) {
+		(UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+	}
+}
